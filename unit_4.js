fad7883ddb1ca14a5a795d3f23951187e8fd5ab2
Matvii Kopchak <matviy.kopchak@gmail.com>
2019-10-27 17:05:41 +0200

    Unit4 task4

--- unit_4.js
+++ unit_4.js
@@ -1,3 +1,9 @@
-document.querySelector('p').onclick = () => {
-  alert("Task 3");
+document.querySelector("button").onclick = () => {
+    var p = document.querySelector("p");
+    var checkbox = document.querySelector("input");
+    console.log(checkbox.checked);
+    if (checkbox.checked == true)
+        p.innerText = "TRUE";
+    else
+        p.innerText = "FALSE";
 };
