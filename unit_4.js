3da509c81bc0d6dad9fd698b72d768909525c781
Matvii Kopchak <matviy.kopchak@gmail.com>
2019-10-27 17:28:00 +0200

    Unit4 task7

--- unit_4.js
+++ unit_4.js
@@ -1,4 +1,6 @@
 document.querySelector("button").onclick = () => {
    var input = document.querySelector("input");
    alert(input.value);
+   if (input.value.length < 6 )
+       console.log("Please write more 6")
 };
