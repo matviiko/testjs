821c0ef59aded243b2ada397b37a03e73d0141c7
Matvii Kopchak <matviy.kopchak@gmail.com>
2019-10-27 18:24:22 +0200

    Unit4 task9

--- unit_4.js
+++ unit_4.js
@@ -1,10 +1,8 @@
-document.querySelector("#but1").onclick =  function ()  {
-   var div = document.querySelector("#div1");
-   div.innerHTML = "<input type='text'>" + "<button>Click</button>";
-    var buttonTwo = document.querySelector("button");
-    buttonTwo.onclick = function () {
-        var input = document.querySelector("input");
-        alert(input.value);
-    }
+document.querySelector("button").onclick =  function ()  {
+   var input = document.querySelector("input");
+   if (input.checked == true)
+       alert(input.value);
+    else
+       alert("false")
 };
-
+// Task 9 unit 4
