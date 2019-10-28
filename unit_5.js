708e69a6bc4911a7ebfcccd9d273e37df3e01144
Matvii Kopchak <matviy.kopchak@gmail.com>
2019-10-28 20:47:42 +0200

    Unit5 task15 false

--- unit_5.js
+++ unit_5.js
@@ -3,11 +3,14 @@ var button = document.querySelector("button");
 button.onclick = function () {
     var p = document.querySelector("p");
     var input =  document.querySelectorAll(".inp");
-    var i = 0;
+    i = 0;
 
+    for (; input.length > i ; i ++)
+    input[3].checked;
     console.log(input.length);
+
 
 };
 
 
-// Lessons 15
+// Lessons 13
