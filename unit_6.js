b6c836c728e85bfaee001c032517072d70acba0e
Matvii Kopchak <matviy.kopchak@gmail.com>
2019-10-29 12:52:48 +0200

    Unit6 task3

--- unit_6.js
+++ unit_6.js
@@ -1,11 +1,10 @@
 var out = document.querySelector('.out');
 
 for (var k=0; k < 3; k++) {
-out.innerHTML += k+1  + "<br>";
     for (var i = 0; i < 3; i++) {
         out.innerHTML += "*_";
     }
     out.innerHTML += "<br>";
 }
 
-// Lessons 2
+// Lessons 3
